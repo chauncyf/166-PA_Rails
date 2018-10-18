00999b3f0bdd78b17450362bf1ce2c3a1b5edf4e
chauncy <chauncyf@qq.com>
2018-10-18 17:24:28 -0400

    test login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # Handle a successful save.
+      log_in @user
       flash[:success] = 'Welcome to Course Catalog'
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'index#home'
 
   # get 'index/home'
@@ -10,6 +11,10 @@ Rails.application.routes.draw do
   get 'users/index'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :courses
   resources :instructors
